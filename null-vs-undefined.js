6269b15b7ddb980d373d189458e7143631ba40dd
shubeen <67527789+shubeen@users.noreply.github.com>
2020-08-07 18:43:48 +1000

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+
+// // no value of variable
+// let pakhi;
+// console.log(pakhi);
+
+
+// // no return 
+// function add ( num1, num2){
+//     console.log(num1+num2);
+// 
+// const result = add ( 13, 12);
+// console.log(result)
+
+
+
+// // return but no parameter to do
+// function add ( num1, num2){
+//     console.log(num1+num2);
+//     return;
+// }
+// const result1 = add (10, 11);
+// console.log(result1)
+
+
+// call but don't pass the value
+function add( num1,num2){
+    console.log(num1, num2);
+}
+const result3 = add ( 2);
+console.log(result3);
+
+let person ={name:'shubeen', phone:645372};
+console.log(person.id);
+
