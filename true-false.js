9deef2f9a41436fbefd5fd8ce26e366c3912fa6a
shubeen <67527789+shubeen@users.noreply.github.com>
2020-08-07 18:06:54 +1000

    true and false condition

---
+++ true-false.js
@@ -0,0 +1,19 @@
+const age = 5;
+if(age){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+const age2 = 0;
+if(age2){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+// 0
+// undefined
+// null / NaN
+// ""  all are false 
